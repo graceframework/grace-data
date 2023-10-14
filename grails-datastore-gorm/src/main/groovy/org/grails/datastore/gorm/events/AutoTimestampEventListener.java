c1c79fab9f78a10158eca0eb497f055379ddcc15
Michael Yan <rain@rainboyan.com>
2023-10-14 18:29:55 +0800

    Adds deprecated annotation

--- grails-datastore-gorm/src/main/groovy/org/grails/datastore/gorm/events/AutoTimestampEventListener.java
+++ grails-datastore-gorm/src/main/groovy/org/grails/datastore/gorm/events/AutoTimestampEventListener.java
@@ -1,10 +1,11 @@
-/* Copyright (C) 2010 SpringSource
+/*
+ * Copyright 2010-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -141,6 +142,7 @@ public class AutoTimestampEventListener extends AbstractPersistenceEventListener
      *
      * @deprecated Use {@link #hasLastUpdated(String)} instead
      */
+    @Deprecated(forRemoval = true, since = "2022.5.0")
     protected boolean hasLastUpdated(PersistentEntity entity) {
         return hasLastUpdated(entity.getName());
     }
@@ -154,6 +156,7 @@ public class AutoTimestampEventListener extends AbstractPersistenceEventListener
      *
      * @deprecated Use {@link #hasDateCreated(String)} instead
      */
+    @Deprecated(forRemoval = true, since = "2022.5.0")
     protected boolean hasDateCreated(PersistentEntity entity) {
         return hasDateCreated(entity.getName());
     }
